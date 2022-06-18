4eff04854587757203dd698e5aca482a1fb4ec8f
vadim.baidyuk <vadim.baidyuk@binibambini.com>
2022-06-18 15:32:05 +0300

    add create folder stream

---
+++ async-education/async-education/UkrainianArtistBiography.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace async_education
+{
+    public class UkrainianArtistBiography
+    {
+        public static readonly Dictionary<string, string> Artists = new Dictionary<string, string>()
+        {
+            ["Malevich"] = "",
+            ["Shevchenko"] = "",
+            ["Aivazovsky"] = "",
+            ["Marchuk"] = "",
+            ["Vrubel"] = "",
+            ["Pimonenko"] = "",
+        };
+    }
+}
