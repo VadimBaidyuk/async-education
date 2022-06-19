08964fe638b0a1e1cf8913284e4715e97dfe853b
vadim.baidyuk <vadim.baidyuk@binibambini.com>
2022-06-19 15:37:03 +0300

    refactoring FileStream

--- async-education/async-education/CreateFileStream.cs
+++ async-education/async-education/CreateFileStream.cs
@@ -10,7 +10,7 @@ namespace async_education
     public class CreateFileStream
     {
         private const string FolderPath = "D:\\_prj\\async-education\\async-education\\async-education\\Artists\\";
-        public static FileStream FileStream { get; private set; }
+        public static StreamWriter StreamWriter { get; private set; }
         
         public static void RunTask()
         {
@@ -22,9 +22,22 @@ namespace async_education
 
         private static readonly Task createFileTask = new Task(CreateFile);
 
-        private static void CreateFile() => 
-            FileStream = File.Create(FolderPath + UkrainianArtistBiography.Artists
-                .ElementAt(new Random().Next(0, UkrainianArtistBiography.Artists.Count)).Key);
-        
+        private static void CreateFile()
+        {
+            var random = new Random();
+            StreamWriter = new StreamWriter(FolderPath + UkrainianArtistBiography.Artists
+                .ElementAt(random.Next(0, UkrainianArtistBiography.Artists.Count)));
+
+            var text = RandomString(random.Next(1, 2000));
+            StreamWriter.WriteLine(text);
+        }
+
+        private static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[new Random().Next(s.Length)]).ToArray());
+        }
     }
 }
